7be79ea6b723d7361affa1044af8c129ac6f6ab1
JeanSummers <hatersparty@gmail.com>
2025-04-10 17:26:26 +0300

    Update 2025-04-10T14:26:26.056Z

---
+++ assets/index-C2QEKkRB.js
@@ -0,0 +1,1 @@
+import{j as t,av as o}from"./index-CtQ3VCww.js";const r=function(){return t.jsx(o,{to:"/creatures"})};export{r as component};
--- assets/index-qbJ3JrM8.js
+++
@@ -1,1 +0,0 @@
-import{j as t,au as o}from"./index-z0nbMM35.js";const r=function(){return t.jsx(o,{to:"/creatures"})};export{r as component};
